85b617f38d04e4ed1ec89e46ab9b7288453476fa
Moha <mimohadinho@gmail.com>
2025-05-26 11:10:50 +0200

    Stage 1/5: ASCII Lands welcome you, Stranger!

---
+++ ASCII Mirror/task/src/asciimirror/Main.java
@@ -0,0 +1,18 @@
+package asciimirror;
+
+public class Main {
+    public static void main(String[] args) {
+        String ascii = """
+                                    _______\s
+                                   < hello >
+                                    -------\s
+                            ^__^   /       \s
+                    _______/(oo)  /        \s
+                /\\/(       /(__)           \s
+                   | w----||               \s
+                   ||     ||               \s
+                """;
+
+        System.out.println(ascii);
+    }
+}
